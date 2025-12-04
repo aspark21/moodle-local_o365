084312449314504769efc07000a48740f6de9143
Alistair Spark <aspark21@users.noreply.github.com>
2025-12-04 10:31:27 +0000

    remove upgrade step that should be a task

--- db/upgrade.php
+++ db/upgrade.php
@@ -1304,24 +1304,5 @@ function xmldb_local_o365_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2024042203, 'local', 'o365');
     }
 
-    if ($oldversion < 2024100711) {
-        // Delete config log entries created by mistake.
-        $confignames = ['apptokens', 'teamscacheupdated', 'calsyncinlastrun', 'task_usersync_lastdeltatoken',
-            'task_usersync_lastdelete', 'cal_site_lastsync', 'cal_course_lastsync', 'cal_user_lastsync'];
-
-        foreach ($confignames as $configname) {
-            // Delete logstore_standard_log records.
-            $DB->delete_records_select('logstore_standard_log',
-                'eventname = :eventname AND objectid IN (SELECT id FROM {config_log} WHERE plugin = :plugin AND name = :name)',
-                ['eventname' => '\core\event\config_log_created', 'plugin' => 'local_o365', 'name' => $configname]);
-
-            // Delete config_log records.
-            $DB->delete_records('config_log', ['plugin' => 'local_o365', 'name' => $configname]);
-        }
-
-        // O365 savepoint reached.
-        upgrade_plugin_savepoint(true, 2024100711, 'local', 'o365');
-    }
-
     return true;
 }
